7a7f83003f8350cd60f082f3c2b4e779e386b8cc
Mateusz Pawlak <mateuszpawlak@interia.pl>
2023-03-17 19:49:18 +0100

    Refaktoryzacja kodu

---
+++ src/main/java/example/MyFrame2.java
@@ -0,0 +1,120 @@
+package example;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.*;
+
+
+
+public class MyFrame2 extends JFrame {
+JLabel label2;
+JPanel panel3, panel4,panel5;
+JTextArea textArea,textArea2;
+
+    MyFrame2(JPanel panel,JPanel panel2, JFrame to,JButton jButton){
+
+        //Wylaczam stare panely
+            panel.setVisible(false);
+            panel2.setVisible(false);
+        //
+
+        //label
+        label2 = new JLabel();
+
+        //panel
+        panel3 = new JPanel();
+        panel3.setBackground(Color.white);
+
+        WpisywanieTekstu wpisywanieTekstu = new WpisywanieTekstu(label2,panel3);
+            to.add(panel3);
+
+    //panel4
+    panel4 = new JPanel();
+            panel4.setBackground(Color.white);
+            panel4.setBounds(45,110,450,100);
+            panel4.setVisible(true);
+    //panel4.setBackground(Color.white);
+            to.add(panel4);
+
+    //panel5
+    panel5 = new JPanel();
+            panel5.setBackground(Color.white);
+            panel5.setBounds(80,210,355,50);
+            panel5.setVisible(true);
+            panel5.setLayout(null);
+    //panel5.setBackground(Color.white);
+    //panel.setBackground(Color.black);
+            to.add(panel5);
+
+            textArea = new JTextArea();
+            textArea.setText("login");
+            textArea.setPreferredSize(new Dimension(210, 40)); // Ta komenda ustala wielkosc okna
+    //textArea.setLineWrap(true);
+            textArea.setWrapStyleWord(true);
+
+    Border border = BorderFactory.createLineBorder(Color.BLACK); // ustaw styl obramowania
+    // Ustaw rozmiar dla JTextField
+    //panel4.setLayout(new GridBagLayout());
+    //textArea.setBackground(Color.gray);
+            textArea.setBorder(border);
+            textArea.setForeground(new Color(0x000000));
+            textArea.setFont(new Font("Consolas", Font.PLAIN,35));
+            textArea.setBounds(0, 0, 340, 60);
+
+            panel4.add(textArea);
+
+    //
+
+    textArea2 = new JTextArea();
+            textArea2.setBorder(border);
+            textArea2.setText("haslo");
+            textArea2.setPreferredSize(new Dimension(210, 40)); // Ta komenda ustala wielkosc okna
+    //textArea.setLineWrap(true);
+            textArea2.setWrapStyleWord(true);
+
+    // Ustaw rozmiar dla JTextField
+    //panel4.setLayout(new GridBagLayout());
+            textArea2.setForeground(new Color(0x000000));
+            textArea2.setFont(new Font("Consolas", Font.PLAIN,35));
+            textArea2.setBounds(0, 0, 340, 60);
+
+            panel4.add(textArea2);
+    //
+
+
+        jButton.setBackground(Color.white);
+        jButton.setVisible(true);
+        jButton.setLayout(null);
+        jButton.setBounds(140,0,100,50);
+        //jButton.addActionListener(to);
+        jButton.setText("Zaloguj");
+        jButton.setFocusable(false);
+        jButton.setBorder(BorderFactory.createEtchedBorder());
+        panel5.add(jButton);
+    }
+
+    public JTextArea getTextArea() {
+        return textArea;
+    }
+
+    public JTextArea getTextArea2() {
+        return textArea2;
+    }
+
+    public JLabel getLabel2() {
+        return label2;
+    }
+
+    public JPanel getPanel3() {
+        return panel3;
+    }
+
+    public JPanel getPanel4() {
+        return panel4;
+    }
+
+    public JPanel getPanel5() {
+        return panel5;
+    }
+}
--- src/main/java/example/login.java
+++ src/main/java/example/login.java
@@ -25,6 +25,9 @@ public class login extends JFrame implements ActionListener {
     JPanel panel,panel2,panel3,panel4,panel5;
     TextField textField;
 
+        //Ustawienia hibernate
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
     //static String Label="Label";
     public login(){
 
@@ -101,124 +104,58 @@ public class login extends JFrame implements ActionListener {
 
         if (e.getSource() == button[0]) {
 
-            //Wylaczam stare panely
-            panel.setVisible(false);
-            panel2.setVisible(false);
-            //
-
-            //label
-            label2 = new JLabel();
-
-            //panel
-            panel3 = new JPanel();
-            panel3.setBackground(Color.white);
-
-            WpisywanieTekstu wpisywanieTekstu = new WpisywanieTekstu(label2,panel3);
-            this.add(panel3);
-
-
-            //panel4
-            panel4 = new JPanel();
-            panel4.setBackground(Color.white);
-            panel4.setBounds(45,110,450,100);
-            panel4.setVisible(true);
-            //panel4.setBackground(Color.white);
-            this.add(panel4);
-
-            //panel5
-            panel5 = new JPanel();
-            panel5.setBackground(Color.white);
-            panel5.setBounds(80,210,355,50);
-            panel5.setVisible(true);
-            panel5.setLayout(null);
-            //panel5.setBackground(Color.white);
-            //panel.setBackground(Color.black);
-            this.add(panel5);
-
-            JTextArea textArea = new JTextArea();
-            textArea.setText("login");
-            textArea.setPreferredSize(new Dimension(210, 40)); // Ta komenda ustala wielkosc okna
-            //textArea.setLineWrap(true);
-            textArea.setWrapStyleWord(true);
-
-            Border border = BorderFactory.createLineBorder(Color.BLACK); // ustaw styl obramowania
-            // Ustaw rozmiar dla JTextField
-            //panel4.setLayout(new GridBagLayout());
-            //textArea.setBackground(Color.gray);
-            textArea.setBorder(border);
-            textArea.setForeground(new Color(0x000000));
-            textArea.setFont(new Font("Consolas", Font.PLAIN,35));
-            textArea.setBounds(0, 0, 340, 60);
-
-            panel4.add(textArea);
-
-            //
-
-            JTextArea textArea2 = new JTextArea();
-            textArea2.setBorder(border);
-            textArea2.setText("haslo");
-            textArea2.setPreferredSize(new Dimension(210, 40)); // Ta komenda ustala wielkosc okna
-            //textArea.setLineWrap(true);
-            textArea2.setWrapStyleWord(true);
-
-            // Ustaw rozmiar dla JTextField
-            //panel4.setLayout(new GridBagLayout());
-            textArea2.setForeground(new Color(0x000000));
-            textArea2.setFont(new Font("Consolas", Font.PLAIN,35));
-            textArea2.setBounds(0, 0, 340, 60);
-
-            panel4.add(textArea2);
-            //
-
             JButton jButton = new JButton();
-            jButton.setBackground(Color.white);
-            jButton.setVisible(true);
-            jButton.setLayout(null);
-            jButton.setBounds(140,0,100,50);
+
+            MyFrame2 frame2 = new MyFrame2(panel,panel2,this,jButton);
+
             jButton.addActionListener(this);
-            jButton.setText("Zapisz");
-            jButton.setFocusable(false);
-            jButton.addActionListener(this);
-            jButton.setBorder(BorderFactory.createEtchedBorder());
-            panel5.add(jButton);
 
             jButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+                public void actionPerformed(ActionEvent e) {
 
-                //Ustawienia hibernate
-                SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+                    Session session = sessionFactory.getCurrentSession();
+                    session.beginTransaction();
 
-                Session session = sessionFactory.getCurrentSession();
-                session.beginTransaction();
+                    boolean isFound = false ;
+                    String text = frame2.getTextArea().getText();
+                    String text2 = frame2.getTextArea2().getText();
+                    System.out.println(text);
+                    List<User> users = session.createQuery("from User", User.class).getResultList();
 
-                String text = textArea.getText();
-                String text2 = textArea2.getText();
-                System.out.println(text);
-                List<User> users = session.createQuery("from User", User.class).getResultList();
+                    for (User user : users) {
+                        if(user.getUsername().equals(text)) {
+                            if(user.getPassword().equals(text2)) {
+                                //frame2.getLabel2().setText("Zalogowano !");
 
-                System.out.println("Użytkownicy w bazie danych:");
-                for (User user : users) {
-                    if(user.getUsername().equals(text)) {
-                        System.out.println(user);
-                        if(user.getPassword().equals(text2)) {
-                            label2.setText("Zalogowano !");
-                            panel3.setVisible(false);
-                            panel4.setVisible(false);
-                            panel5.setVisible(false);
-                            panel.setVisible(true);
-                            panel2.setVisible(true);
+                                //
 
-                            button[0].setText("Nowy trening");
-                            button[1].setText("Edytuj trening");
-                            button[2].setText("Wpisz mi 3 z PB");
-                            break;
+                                frame2.getPanel3().setVisible(false);
+                                frame2.getPanel4().setVisible(false);
+                                frame2.getPanel5().setVisible(false);
+
+                                panel.setVisible(true);
+                                panel2.setVisible(true);
+
+                                button[0].setText("Nowy trening");
+                                button[1].setText("Edytuj trening");
+                                button[2].setText("Wpisz mi 3 z PB");
+                                //
+                                break;
+                            } else {
+                                frame2.getLabel2().setText("Bledne haslo !");
+                                isFound = true;
+                                break;
+                            }
                         }
                     }
+                    if(isFound==false)
+                    frame2.getLabel2().setText("Bledny login !");
+                    isFound = false;
+                    session.getTransaction().commit();
                 }
+            });
 
-                session.getTransaction().commit();
-            }
-        });
+
 
         }
         if (e.getSource() == button[2]) {
