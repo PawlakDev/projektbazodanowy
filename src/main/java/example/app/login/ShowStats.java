3eb699ce1c680aa783131f8c0e221a16cccb6ad0
korsem <korneliasem@gmail.com>
2023-06-14 20:20:35 +0200

    show stats #2

--- src/main/java/example/app/login/ShowStats.java
+++ src/main/java/example/app/login/ShowStats.java
@@ -1,8 +1,17 @@
 package example.app.login;
 
 import example.app.dbSettings.User;
+import example.app.dbSettings.Workouts;
 import org.hibernate.SessionFactory;
+import example.app.WorkoutRepository;
 
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
@@ -15,6 +24,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import static example.app.Start.*;
 
@@ -32,38 +42,79 @@ public class ShowStats extends JFrame{
         headlinePanel.setBackground(Color.white);
         LoginInfoFrameSettings loginInfoFrameSettings = new LoginInfoFrameSettings(headlineLabel,headlinePanel, "Twoje statystyki");
 
-        // Tworzenie przykladowego zestawu danych
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(5, "Training", "Monday");
-        dataset.addValue(10, "Training", "Tuesday");
-        dataset.addValue(8, "Training", "Wednesday");
-        dataset.addValue(3, "Training", "Thursday");
-        dataset.addValue(6, "Training", "Friday");
+        WorkoutRepository repository = new WorkoutRepository(sessionFactory);
+        List<Workouts> workouts = repository.getWorkoutsByUserId(currentUser.getId());
+
+        // kalendarz to musze do innej klasy przeniesc
+        Calendar calendar = Calendar.getInstance();
+
+        // pobieram moje dane z tabel do kilometrów
+        DefaultCategoryDataset datasetKilometers = new DefaultCategoryDataset();
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        for (int i = 0; i < 5; i++) {
+            LocalDate weekStart = currentDate.minusWeeks(i).with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+            LocalDate weekEnd = currentDate.minusWeeks(i).with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+
+            int sumKilometers = workouts.stream()
+                    .filter(workout -> {
+                        LocalDate workoutDate = LocalDate.parse(workout.getDate(), formatter);
+                        return !workoutDate.isBefore(weekStart) && !workoutDate.isAfter(weekEnd);
+                    })
+                    .mapToInt(Workouts::getKilometers)
+                    .sum();
+
+            datasetKilometers.addValue(sumKilometers, "Training", weekStart.format(formatter));
+        }
 
         // Konwersja na CategoryDataset
-        CategoryDataset categoryDataset = dataset;
+        CategoryDataset categoryDataset = datasetKilometers;
 
         // Tworzenie wykresu
-        JFreeChart chart = ChartFactory.createBarChart(
-                "Training Statistics Demo",     // Tytuł wykresu
-                "Day",                     // Etykieta osi X
-                "Number of Trainings",     // Etykieta osi Y
+        JFreeChart chartKilometers = ChartFactory.createBarChart(
+                "Kilometry w dany tydzień",     // Tytuł wykresu
+                "tydzień",                     // Etykieta osi X
+                "suma kilometrów",     // Etykieta osi Y
                 categoryDataset,           // Zestaw danych
                 PlotOrientation.VERTICAL,  // Orientacja wykresu
-                true,                      // Wyświetlanie legendy
-                true,                      // Wyświetlanie wskazówek
+                false,                      // Wyświetlanie legendy
+                false,                      // Wyświetlanie wskazówek
                 false                      // Wyłączenie URL-ów
         );
 
         // Tworzenie panelu wykresu i dodanie do niego wykresu
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setVisible(true);
-        chartPanel.setBounds(60,90,415,190);
+        ChartPanel chartKilometersPanel = new ChartPanel(chartKilometers);
+        chartKilometersPanel.setVisible(true);
+        chartKilometersPanel.setBounds(60,90,270,100);
+        chartKilometersPanel.setOpaque(false);
 
-        // Dodanie panelu wykresu do głównego panelu
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setVisible(true);
+        // pobieram moje dane z tabel czas
+        DefaultCategoryDataset datasetTime = new DefaultCategoryDataset();
+        for (Workouts workout : workouts) {
+            datasetTime.addValue(workout.getTimeworkout(), "Training", workout.getDate());
+        }
+
+        // Konwersja na CategoryDataset
+        CategoryDataset categoryDataset2 = datasetKilometers;
+
+        // Tworzenie wykresu
+        JFreeChart chartTime = ChartFactory.createBarChart(
+                "Ilość minut w dany dzień",     // Tytuł wykresu
+                "Dzień",                     // Etykieta osi X
+                "Ilosc minut",     // Etykieta osi Y
+                categoryDataset,           // Zestaw danych
+                PlotOrientation.VERTICAL,  // Orientacja wykresu
+                false,                      // Wyświetlanie legendy
+                false,                      // Wyświetlanie wskazówek
+                false                      // Wyłączenie URL-ów
+        );
+
+        // Tworzenie panelu wykresu i dodanie do niego wykresu
+        ChartPanel chartTimePanel = new ChartPanel(chartKilometers);
+        chartTimePanel.setVisible(true);
+        chartTimePanel.setBounds(60,205,270,100);
+        chartTimePanel.setOpaque(false);
 
         // button cofania
         JButton buttonBack = new JButton();
@@ -82,6 +133,7 @@ public class ShowStats extends JFrame{
         buttonBack.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
+                buttonBack.setVisible(false);
                 getBackgroundImagePanel().setVisible(true);
                 getButtonPanel().setVisible(true);
                 getjLayeredPane().setVisible(true);
@@ -91,8 +143,8 @@ public class ShowStats extends JFrame{
 
         //obecne elementy dodaje do frema
         to.add(buttonBack);
-        to.add(mainPanel);
         to.add(headlinePanel);
-        to.add(chartPanel);
+        to.add(chartKilometersPanel);
+        to.add(chartTimePanel);
     }
 }
